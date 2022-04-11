b1aa55ed970a0ca6d1fd6ec2dec3e801b911d56e
Dan Violet Sagmiller <Game.Dev@Live.com>
2022-04-10 22:31:25 -0700

    Terrain Height map Setup level 1

--- Assets/!/Scripts/TerrainModLayers/HeightPainterTerrainModLayer.cs
+++ Assets/!/Scripts/TerrainModLayers/HeightPainterTerrainModLayer.cs
@@ -1,10 +1,23 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEngine;
+#endif // UNITY_EDITOR
 
 [CreateAssetMenu(menuName = "Terrain/Layers/Paint By Height")]
 public class HeightPainterTerrainModLayer : TerrainModLayer
 {
+    [System.Serializable]
+    public class TopologyLayer
+    {
+        public int TargetHeight = 0;
+        public int Index = 0;
+    }
+
     public float[,,] Splat = null;
-    public int SquareSize = 1;
+    public List<TopologyLayer> Topologies;
 
     public void OnValidate()
     {
@@ -13,22 +26,38 @@ public class HeightPainterTerrainModLayer : TerrainModLayer
 
     public override void Rebuild()
     {
-        Splat = new float[
-            Tool._TerrainData.alphamapWidth, 
-            Tool._TerrainData.alphamapHeight, 
-            Tool._TerrainData.alphamapLayers];
+        var maxX = Tool._TerrainData.alphamapWidth;
+        var maxY = Tool._TerrainData.alphamapHeight;
+        var layerCount = Tool._TerrainData.alphamapLayers;
+        Splat = new float[maxX, maxY, layerCount];
 
-        var doubleSize = SquareSize * 2;
-
+        Topologies.Sort((a, b) => a.TargetHeight - b.TargetHeight);
+        bool usedTopology = false;
         for (int x = 0; x < Tool._TerrainData.alphamapWidth; x++)
         {
             for (int y = 0; y < Tool._TerrainData.alphamapHeight; y++)
             {
-                var xOn = (x % doubleSize) + 1 > SquareSize;
-                var yOn = (y % doubleSize) + 1 > SquareSize;
-                //if (xOn) yOn = !yOn;
-                if (yOn && !xOn || !yOn && xOn) Splat[x, y, 0] = 1;
-                else Splat[x, y, 1] = 1;
+                float percentX = (float)x / (float)maxX;
+                float percentY = (float)y / (float)maxY;
+                float height = Tool._TerrainData.GetHeight(
+                    Mathf.RoundToInt(percentY * Tool.HeightRes), 
+                    Mathf.RoundToInt(percentX * Tool.HeightRes));
+
+                usedTopology = false;
+                for (int i = 1; i < Topologies.Count; i++)
+                {
+                    if(Topologies[i].TargetHeight < height)
+                    {
+                        usedTopology = true;
+                        Splat[x, y, Topologies[i - 1].Index] = 1;
+                        break;
+                    }
+                }
+
+                if (!usedTopology)
+                {
+                    Splat[x, y, Topologies[Topologies.Count - 1].Index] = 1;
+                }
             }
         }
     }
